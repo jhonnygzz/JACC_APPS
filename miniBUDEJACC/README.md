d603c842d8c8d2e30680bafef718470cd3ff69c8
alexishuante <alexis.huante@gmail.com>
2024-11-27 13:54:17 -0500

    small adjustments

--- miniBUDEJACC/README.md
+++ miniBUDEJACC/README.md
@@ -4,14 +4,16 @@ Make sure you are in the miniBUDEJACC directory.
 
 Run these commands: 
 ```shell
+julia --project=. -e 'import Pkg; Pkg.instantiate()'
+
 julia --project=.
 using JACC.JACCPreferences
 JACCPreferences.set_backend("amdgpu")
+exit()
+
+julia --project=.
 using JACC
-@show JACC.JACCPreferences.backend  # Should show "amdgpu"
-
-julia --project=. -e 'import Pkg; Pkg.instantiate()'
-
+@show JACC.JACCPreferences.backend # should show "amdgpu"
 exit()
 
 julia --project=. src/JACCBUDE.jl -p 2 --deck src/data/bm2
--- miniBUDEJACC/scripts_frontier/config_frontier_JACC.sh
+++ miniBUDEJACC/scripts_frontier/config_frontier_JACC.sh
@@ -9,6 +9,10 @@ module purge
 # Load required modules
 module load Core/24.07 #needed to load julia in frontier.
 module load julia/1.10.2
+module load rocm/6.2.0
+
+# Install and set up packages
+julia --project=$PROJ_DIR -e 'import Pkg; Pkg.instantiate()'
 
 # Set backend preference
 julia --project=$PROJ_DIR -e '
@@ -17,9 +21,6 @@ JACCPreferences.set_backend("amdgpu")
 using JACC
 @show JACC.JACCPreferences.backend  # Should show "amdgpu"
 '
-
-# Install and set up packages
-julia --project=$PROJ_DIR -e 'import Pkg; Pkg.instantiate()'
 
 echo "Configuration complete."
 
